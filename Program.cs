eedda4ec3bb5495eb0b697f3627fc569fa40fc02
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-12 08:24:44 +0200

    basic app data - add ingredient effects - output counts as verification

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        var data = IngredientEffects.data;
+        Console.WriteLine($"There are {data.Count} ingredients with a total of {data.SelectMany(i => i.Value).Distinct().Count()} unique effects");
+    }
+}
