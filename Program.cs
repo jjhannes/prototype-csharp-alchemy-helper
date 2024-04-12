b6c76dcc9f23930a5a5420b73b0a5a2b1c79f990
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-12 08:47:36 +0200

    create new method - is bad effect

--- Program.cs
+++ Program.cs
@@ -30,4 +30,23 @@ internal partial class Program
             .Select(i => i.Key)
             .ToArray();
     }
+
+    private static bool IsBadEffect(string effect)
+    {
+        effect = effect.ToLower();
+
+        if (effect.Contains("cure") || effect.Contains("resist"))
+        {
+            return false;
+        }
+
+        return effect.Contains("burden") ||
+            effect.Contains("poison") ||
+            effect.Contains("blind") ||
+            effect.Contains("damage") ||
+            effect.Contains("drain") ||
+            effect.Contains("paralyz") ||
+            effect.Contains("weakness") ||
+            effect.Contains("vampirism");
+    }
 }
