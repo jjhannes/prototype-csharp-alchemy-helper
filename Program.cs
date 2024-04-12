130fbb62d1b032fb649b37f06a829ce66e473747
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-12 08:31:53 +0200

    define new method - get effects for given ingredient

--- Program.cs
+++ Program.cs
@@ -6,4 +6,11 @@ internal partial class Program
     {
         Console.WriteLine($"There are {data.Count} ingredients with a total of {data.SelectMany(i => i.Value).Distinct().Count()} unique effects");
     }
+
+    private static string[] GetEffectsForIngredient(string ingredient)
+    {
+        return data
+            .Single(i => i.Key == ingredient)
+            .Value;
+    }
 }
