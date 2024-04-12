5559ec8617d23060294a9573e67822ae1379cd04
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-12 08:44:20 +0200

    test new methods - get effects for ingredients - get ingredients with effects

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@ internal partial class Program
     private static void Main(string[] args)
     {
         Console.WriteLine($"There are {data.Count} ingredients with a total of {data.SelectMany(i => i.Value).Distinct().Count()} unique effects");
+
+        string rawEbonyIngredient = "Raw Ebony";
+        string[] effectsWaterWalkingAndFortifySpeed = [ "Water Walking", "Fortify Speed" ];
+
+        string[] rawEbonyEffects = Program.GetEffectsForIngredient(rawEbonyIngredient);
+        string[] ingredientsWithWaterWalkingAndFortifySpeed = GetIngredientsWithEffects(effectsWaterWalkingAndFortifySpeed);
+
+        Console.WriteLine($"{rawEbonyIngredient} has effects [{string.Join(", ", rawEbonyEffects)}]");
+        Console.WriteLine($"Ingredients with effects [{string.Join(" & ", effectsWaterWalkingAndFortifySpeed)}] are [{string.Join(", ", ingredientsWithWaterWalkingAndFortifySpeed)}]");
     }
 
     private static string[] GetEffectsForIngredient(string ingredient)
