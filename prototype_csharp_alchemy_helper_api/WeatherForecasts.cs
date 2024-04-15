7207cbc4ad8df8d1fa3b62f944f6405cc0df761d
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-15 16:01:51 +0200

    non-functional changes - add VS *.csproj.user to gitignore - add preliminary mediator implementation configuration entry to appsettings - move dummy weatherforecast controller to its own file and call from program - add domain projects reference to api project - update app port launch settings

---
+++ prototype_csharp_alchemy_helper_api/WeatherForecasts.cs
@@ -0,0 +1,27 @@
+﻿namespace prototype_csharp_alchemy_helper_api;
+
+public class WeatherForecasts
+{
+    private static string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static void ConfigureRoutes(WebApplication app)
+    {
+        app.MapGet("/weatherforecast", () =>
+        {
+            var forecast =  Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi();
+    }
+}
